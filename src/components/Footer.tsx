fd9a8432732ecf57f26172810d1e41ef0430e5ed
fzsf163 <yofaisol619@gmail.com>
2023-10-23 15:43:16 +0600

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className=" border-t-2 border-slate-400 w-full text-center py-4">
+      &copy; FancyFinger__Anime__Explore
+    </footer>
+  );
+}
