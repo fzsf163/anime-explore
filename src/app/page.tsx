76bf79f14526371d596028c45ca9321aacee3db1
fzsf163 <yofaisol619@gmail.com>
2023-10-25 02:48:57 +0600

    get another set of anime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,172 +1,75 @@
-import Footer from "@/components/Footer";
+import MasonaryTile from "@/components/MasonaryTile";
 import TopAnime from "@/components/TopAnime";
-
+import { TopAnimeType } from "@/types";
 async function getData() {
   const res = await fetch(
-    "https://api.jikan.moe/v4/top/anime?sfw&filter=airing",
-    {
-      next: { revalidate: 1 },
-    }
+    "https://api.jikan.moe/v4/top/anime?sfw&filter=airing"
   );
   const data = res.json();
   return data;
 }
-export type TopAnime = {
-  data: [
-    {
-      mal_id: number;
-      url: string;
-      images: {
-        jpg: {
-          image_url: string;
-          small_image_url: string;
-          large_image_url: string;
-        };
-        webp: {
-          image_url: string;
-          small_image_url: string;
-          large_image_url: string;
-        };
-      };
-      trailer: {
-        youtube_id: string;
-        url: string;
-        embed_url: string;
-      };
-      approved: true;
-      titles: [
-        {
-          type: string;
-          title: string;
-        }
-      ];
-      title: string;
-      title_english: string;
-      title_japanese: string;
-      title_synonyms: [string];
-      type: string;
-      source: string;
-      episodes: number;
-      status: string;
-      airing: true;
-      aired: {
-        from: string;
-        to: string;
-        prop: {
-          from: {
-            day: number;
-            month: number;
-            year: number;
-          };
-          to: {
-            day: number;
-            month: number;
-            year: number;
-          };
-          string: string;
-        };
-      };
-      duration: string;
-      rating: string;
-      score: number;
-      scored_by: number;
-      rank: number;
-      popularity: number;
-      members: number;
-      favorites: number;
-      synopsis: string;
-      background: string;
-      season: string;
-      year: number;
-      broadcast: {
-        day: string;
-        time: string;
-        timezone: string;
-        string: string;
-      };
-      producers: [
-        {
-          mal_id: number;
-          type: string;
-          name: string;
-          url: string;
-        }
-      ];
-      licensors: [
-        {
-          mal_id: number;
-          type: string;
-          name: string;
-          url: string;
-        }
-      ];
-      studios: [
-        {
-          mal_id: number;
-          type: string;
-          name: string;
-          url: string;
-        }
-      ];
-      genres: [
-        {
-          mal_id: number;
-          type: string;
-          name: string;
-          url: string;
-        }
-      ];
-      explicit_genres: [
-        {
-          mal_id: number;
-          type: string;
-          name: string;
-          url: string;
-        }
-      ];
-      themes: [
-        {
-          mal_id: number;
-          type: string;
-          name: string;
-          url: string;
-        }
-      ];
-      demographics: [
-        {
-          mal_id: number;
-          type: string;
-          name: string;
-          url: string;
-        }
-      ];
-    }
-  ];
-  pagination: {
-    last_visible_page: number;
-    has_next_page: true;
-    items: {
-      count: number;
-      total: number;
-      per_page: number;
-    };
-  };
-};
+async function getTopAnime() {
+  const res = await fetch(
+    "https://api.jikan.moe/v4/top/anime?sfw&filter=bypopularity"
+  );
+  const data = res.json();
+  return data;
+}
+
 export default async function Home() {
-  const data: TopAnime = await getData();
+  const data: TopAnimeType = await getData();
+  const popular: TopAnimeType = await getTopAnime();
   const airing = data.data;
-  // console.log(airing[0]);
+  const popularData = popular.data;
+  // console.log(popularData[0]);
   return (
-    <section className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 p-5 w-fit">
-      {airing.map((air) => {
-        return (
-          <TopAnime
-            key={air.mal_id}
-            img={air.images.jpg.image_url}
-            title={air.title}
-          ></TopAnime>
-        );
-      })}
-    </section>
+    <main>
+      <section className="flex items-start justify-center mt-10 ">
+        <div className="w-fit sticky top-10 pe-2">
+          <h1 className="text-center text-4xl font-mono font-extrabold animate-pulse ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="100"
+              height="100"
+              viewBox="0 0 24 24"
+            >
+              <g
+                id="feTrophy0"
+                fill="none"
+                fillRule="evenodd"
+                stroke="none"
+                strokeWidth="1"
+              >
+                <g id="feTrophy1" fill="currentColor">
+                  <path
+                    id="feTrophy2"
+                    d="M6.207 9H6a2 2 0 1 1 0-4V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v1a2 2 0 1 1 0 4h-.207A5.504 5.504 0 0 1 13 12.978V17h1a2 2 0 0 1 2 2v1h1a1 1 0 0 1 0 2H7a1 1 0 0 1 0-2h1v-1a2 2 0 0 1 2-2h1v-4.022A5.504 5.504 0 0 1 6.207 9ZM8 4v3.5a3.5 3.5 0 0 0 3.5 3.5h1A3.5 3.5 0 0 0 16 7.5V4H8Z"
+                  />
+                </g>
+              </g>
+            </svg>
+          </h1>
+          <hr className="mt-5 opacity-25" />
+          <hr className="mt-5 opacity-25" />
+        </div>
+        {/* popular */}
+        <section>
+          <MasonaryTile data={popularData}></MasonaryTile>
+        </section>
+      </section>
+
+      {/* airing */}
+      <section className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 p-5 w-fit">
+        {airing.map((air) => {
+          return (
+            <TopAnime
+              key={air.mal_id}
+              img={air.images.jpg.image_url}
+              title={air.title}
+            ></TopAnime>
+          );
+        })}
+      </section>
+    </main>
   );
 }
