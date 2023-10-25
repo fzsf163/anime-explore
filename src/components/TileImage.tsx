a99cdff94cdefe40d46e96647ef2837fa5743ed3
fzsf163 <yofaisol619@gmail.com>
2023-10-26 01:10:25 +0600

    making the on tap work

--- src/components/TileImage.tsx
+++ src/components/TileImage.tsx
@@ -21,23 +21,26 @@ export default function TileImage({ ...porps }: Props) {
       animate="show"
       exit="hidden"
     >
-      <motion.section key={porps.mal_id} variants={porps.variant}>
+      <motion.section key={porps.mal_id} variants={porps.variant} className="space-y-4 p-3 ">
         <motion.div
           initial={{ height: 0, opacity: 0 }}
-          animate={{ height: isHovered ? 100 : 0, opacity: isHovered ? 1 : 0 }}
+          animate={{
+            height: isHovered ? "fit-content" : 0,
+            opacity: isHovered ? 1 : 0,
+          }}
         >
           <h1>{porps.title}</h1>
           <h1>{porps.title_japanese}</h1>
-          <h2>{porps.title_english}</h2>
+          {/* <h2>{porps.title_english}</h2> */}
         </motion.div>
         <motion.img
-          whileInView={{ opacity: 1, transition: { duration: 1.3 } }}
+          whileInView={{ opacity: 1, transition: { duration: 2.3 } }}
           onTap={() => setHovered((x) => !x)}
           src={porps.img}
           alt={porps.title}
           width={500}
           height={400}
-          className="w-fit h-max object-cover rounded-md shadow-lg shadow-slate-700 "
+          className="w-fit h-max object-cover rounded-md shadow-lg shadow-slate-700"
         />
       </motion.section>
     </motion.div>
